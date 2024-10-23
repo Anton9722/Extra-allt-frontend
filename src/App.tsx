878fefad0e3ea00ad71458e30affa93620efeda9
Anton Johansson <anton9797johansson@hotmail.com>
2024-10-23 16:01:51 +0200

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Signup from './components/Signup'
 import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
